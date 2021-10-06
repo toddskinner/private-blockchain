56772d15b29b5f0eb713a82cff62336a382ac2b1
Todd <todd@useequipped.com>
2021-10-05 23:54:24 -0700

    remove gitignore files

--- node_modules/tiny-secp256k1/lib/rand.js
+++
@@ -1,7 +0,0 @@
-import { randomBytes } from "crypto";
-export function generateInt32() {
-    return randomBytes(4).readInt32BE(0);
-}
-export function generateSeed() {
-    return randomBytes(32);
-}
